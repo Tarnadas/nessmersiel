2569705aad4614f64482567366a46f658f803373
Mario Reder <m_reder10@cs.uni-kl.de>
2019-01-07 09:44:29 +0100

    change contact information

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,23 +8,21 @@ const Contact = (props) => (
                     <div className="contact-method">
                         <span className="icon alt fa-envelope"></span>
                         <h3>Email</h3>
-                        <a href="#">abc@def.de</a>
+                        <a href="mailto:harry.holschuh@web.de">harry.holschuh@web.de</a>
                     </div>
                 </section>
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-phone"></span>
-                        <h3>Phone</h3>
-                        <span>(000) 000-0000 x12387</span>
+                        <h3>Telefon</h3>
+                        <span>+49(0)151 22413319</span>
                     </div>
                 </section>
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-home"></span>
-                        <h3>Address</h3>
-                        <span>1234 Somewhere Road #5432<br />
-                        Nashville, TN 00000<br />
-                        United States of America</span>
+                        <h3>Adresse</h3>
+                        <span>Nessmersiel</span>
                     </div>
                 </section>
             </section>
